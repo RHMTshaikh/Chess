3a6789d787cfecf026cf08dfa73277ae16490211
RHMTshaikh <rahmatsheikh99@gmail.com>
2024-07-12 22:08:49 +0530

    const PORT = process.env.PORT || 4000

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -40,7 +40,8 @@ app.use('/api/user', userRoutes);
     });
     
     passConnection(pool);
-	app.listen(process.env.PORT, () => {
-		console.log('Listening on port', process.env.PORT);
+    const PORT = process.env.PORT || 4000
+	app.listen(PORT, () => {
+		console.log('Listening on port', PORT);
 	});
 })();
