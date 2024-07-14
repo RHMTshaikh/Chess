0623192350a00dcfbc11fd566e18c62f665c63da
RHMTshaikh <rahmatsheikh99@gmail.com>
2024-07-14 19:56:59 +0530

    .

--- frontend/src/context/AuthContext.tsx
+++ frontend/src/context/AuthContext.tsx
@@ -36,6 +36,8 @@ export const authReducer = (state: AuthState, action: AuthAction): AuthState => 
     switch (action.type) {
         case 'LOGIN':
             localStorage.setItem('user', JSON.stringify(action.payload));
+            console.log(`email=${action.payload.email}; domain=${process.env.REACT_APP_SERVER_URL?.substring(7)}; path=/; SameSite=None; Secure`);
+            
             document.cookie = `email=${action.payload.email}; domain=${process.env.REACT_APP_SERVER_URL?.substring(7)}; path=/; SameSite=None; Secure`;
             return { user: action.payload };
         case 'LOGOUT':
